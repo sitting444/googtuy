e7df1679d4f7b8cf07491060ef1972f8bb6382c0
sitting444 <59213177+sitting444@users.noreply.github.com>
2020-08-01 20:38:49 +0700

    Initialize ellipsis

---
+++ models/bitcoinUnits.js
@@ -0,0 +1,10 @@
+export const BitcoinUnit = Object.freeze({
+  BTC: 'BTC',
+  SATS: 'sats',
+  LOCAL_CURRENCY: 'local_currency',
+});
+
+export const Chain = Object.freeze({
+  ONCHAIN: 'ONCHAIN',
+  OFFCHAIN: 'OFFCHAIN',
+});
